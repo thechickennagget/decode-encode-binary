0e4ee3ab53b27d53cbcdeadfbe2a3c7c6661abd8
ChickenNaggetGitHub <54007132+ChickenNaggetGitHub@users.noreply.github.com>
2019-08-15 02:24:53 +0200

    Update to 0.1.0

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-"use strict";
 require("./index.js");
 var version = require("./package.json").version;
 module.exports = {
   decode: function(binary){
-  if (!binary) { return 'No numbers to decode was provided'; }
-  else if (binary === ' ') { 'Text cannot be empty string'; }; exports.done = false;
+  if(typeof binary === "number"){ throw new Error("please add ' ' tags to the binary numbers, for example: test.decode('000101101') instead of the: test.decode(000101101)"); };
+  if(typeof binary === "string"){
+  if (!binary) { throw new Error('No Text to decode was provided'); }
+  else if (binary === ' ') { throw new Error('Text cannot be empty string'); };
   try{ function toBinary(tobin) { tobin = tobin.replace(/\s+/g, ''); tobin = tobin.match(/.{1,8}/g).join(' ');
   return tobin.split(" ").map(function (elem) { return String.fromCharCode(parseInt(elem, 2)); }).join(""); }; exports.done = true;
-  return toBinary(binary); }catch(err){ console.error(err.stack); };
+  return toBinary(binary).toString(); }catch(err){ throw new Error(err.stack); };
+   }
   },
 
 encode: function(text){
-  if (!text) { return 'No text to encode was provided'; }
-  else if (text === ' ') { return 'Text cannot be empty string'; }; exports.done = false;
+  if (!text) { throw new Error('No Text to encode was provided'); }
+  else if (text === ' ') { throw new Error('Text cannot be empty string'); }; 
   try{ function toText(str, spaceSeparatedOctets) { function zeroPad(num) { return '00000000'.slice(String(num).length) + num; }
   return str.replace(/[\s\S]/g, (str) => { str = zeroPad(str.charCodeAt().toString(2));
-  return !1 === spaceSeparatedOctets ? str : `${str} `; }); }; exports.done = true;
-  return toText(text); }catch(err){ console.error(err.stack); };
+  return !1 === spaceSeparatedOctets ? str : `${str} `; }) }; exports.done = true;
+  return toText(text).toString(); }catch(err){ throw new Error(err.stack); };
   },
   
-version: function(){ try{ return version; }catch(err){ console.log(err.stack)};
-   }
-};
+version: function() { try{ return version; }catch(err){ console.log(err.stack)};
+   },  
+
+   auto: function(detect){
+      if (detect.startsWith("0" || "1") || detect.endsWith("1" || "0")){
+     //Binary detected
+        if (!detect) { throw new Error('No Text to decode was provided'); }
+        else if (detect === ' ') { throw new Error('Text cannot be empty string'); };
+        try{ function toBinary(tobin) { tobin = tobin.replace(/\s+/g, ''); tobin = tobin.match(/.{1,8}/g).join(' ');
+        return tobin.split(" ").map(function (elem) { return String.fromCharCode(parseInt(elem, 2)); }).join(""); }; exports.done = true;
+        return toBinary(detect).toString(); }catch(err){ throw new Error(err.stack); };
+      }else{
+
+    //Text detecteed
+        if (!detect) { throw new Error('No Text to encode was provided'); }
+        else if (detect === ' ') { throw new Error('Text cannot be empty string'); }; 
+        try{ function toText(str, spaceSeparatedOctets) { function zeroPad(num) { return '00000000'.slice(String(num).length) + num; }
+        return str.replace(/[\s\S]/g, (str) => { str = zeroPad(str.charCodeAt().toString(2));
+        return !1 === spaceSeparatedOctets ? str : `${str} `; }) }; exports.done = true;
+        return toText(detect).toString(); }catch(err){ throw new Error(err.stack); };
+      }
+      }
+    };
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ## Decode and encode text to binary 
-
-### An example decoding text to binary numbers
+### An example decoding text from binary numbers
+------
 ```
 const test = require("decode-and-encode-binary-text")
 console.log(test.decode("0110100001100101011011000110110001101111001000000111011101101111011100100110110001100100"))
@@ -8,10 +8,27 @@ console.log(test.decode("0110100001100101011011000110110001101111001000000111011
 // outputs "hello world"
 ```
 
-### Example encoding text from binary numbers
+### Example encoding text to binary numbers
+------
 ```
 const test = require("decode-and-encode-binary-text")
 console.log(test.encode("Hello world!"))
 
 // outputs "01101000 01100101 01101100 01101100 01101111 01110111 01101111 01110010 01101100 01100100"
  ```
+### Auto Detecting
+##### Automatically detect if you want to encode or decode
+------
+##### Warning!! this is experimental feature!! 
+
+```
+const test = require("decode-and-encode-binary-text")
+
+console.log(test.auto("ok"))
+// outputs 01101111 01101011
+
+console.log(test.auto("01101111 01101011"))
+// outputs "ok"
+```
+
+ > if you need help, open new issue in github repository
