abb416fef6fedcbcd32c459430ed480f11eec2b2
TheChickenNagget <54007132+TheChickenNagget@users.noreply.github.com>
2019-09-05 18:59:28 +0200

    Fixed some things

--- src/cli-loader.js
+++ src/cli-loader.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 'use strict';
+try{
 var argv = process.argv.slice(2).toString();
-argv = argv.replace(",", " ")
-
 if(!argv){
  console.error(`No Command was provided so here are all commands`)
  console.log(`
@@ -11,18 +10,13 @@ if(!argv){
   - decode ~ Decode provided string from binary
  `)
 }
-try{
 if(argv.search("encode") === 0){
 argv = argv.replace('encode','');
 console.log(require("./index.js").encode(`${argv}`))
-process.exit(0)
-}
+process.exit(0)}
 
 else if(argv.search("decode") === 0){
-argv = argv.replace('decode','');
+argv = argv.replace('decode',''); 
 console.log(require("./index.js").decode(`${argv}`))
-process.exit(0)
-}
- }catch(err){
-     throw new Error(err)
- }
+process.exit(0);
+}}catch(err){ throw new Error(err) };
