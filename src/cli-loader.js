59563c8da5aacbd8baf0d5a5c705739c2556eef5
TheChickenNagget <54007132+TheChickenNagget@users.noreply.github.com>
2019-09-18 15:45:42 +0200

    Add files via upload

--- src/cli-loader.js
+++ src/cli-loader.js
@@ -5,7 +5,7 @@ var argv = process.argv.slice(2).toString();
 argv = argv.replace(",", "")
 try{
 if(argv.search("encode")===0){argv=argv.replace('encode','');console.log(require("./index.js").encode(`${argv}`));process.exit(0);}
-if(argv.search("decode")===0){argv=argv.replace('decode','');console.log(require("./index.js").decode(`${argv}`));process.exit(0);}
+if(argv.search("decode")===0){argv=argv.replace('decode','');if(argv === null ||  /^\s*$/.test(argv)){return console.error("> No Text to decode was provided");} console.log(require("./index.js").decode(`${argv}`));process.exit(0);}
 if(!argv){console.log(`
                 Commands
 - encode ~ Encode provided string to binary
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 "use strict";
-/* This Code is abit minified to save space */
 require("v8-compile-cache");
 module.exports = {
 /**
@@ -7,13 +6,13 @@ module.exports = {
  * @param {string} binary
  * @example 
  * 
- * const test = require("decode-and-encode-binary-text")
+ * const test = require("decode-encode-binary")
  * console.log(test.decode("0100100001100101011011000110110001101111"))
  * // outputs "Hello"
  */
 decode:function(b){
-if(!b){throw new Error('No Text to decode / encode was provided');}else if(b===' '){throw new Error('Text cannot be empty string');}			 
-if(typeof b==="number"){throw new Error("text to encode/decode must be string");}	
+if(!b ||  /^\s*$/.test(b)){throw new Error('No Text to decode was provided');}
+if(typeof b==="number"){throw new Error("text to decode must be string");}	
 try{function tobin(b){b=b.replace(/\s+/g,'');b=b.match(/.{1,8}/g).join(' ');
 return b.split(" ").map(function(m){return String.fromCharCode(parseInt(m,2));}).join("");}return tobin(b).toString();
 }catch(e){
@@ -24,12 +23,12 @@ if(e.message==="Cannot read property 'join' of null"){throw new Error("Text to d
  * @param {string} text
  * @param {boolean} spaces
  * @example
- * var test = require("decode-and-encode-binary-text")
+ * var test = require("decode-encode-binary")
  * console.log(test.encode("Hello"))
  * // outputs "0100100001100101011011000110110001101111"
  * 
  * // second example using "spaces"
- * var test = require("decode-and-encode-binary-text")
+ * var test = require("decode-encode-binary")
  * console.log(test.encode("Hello", true))
  * // outputs "01001000 01100101 01101100 01101100 01101111"
  */
@@ -43,7 +42,7 @@ return totxt(t).toString();}catch(e){throw new Error(`Error ${e.stack}`);}},
 /**
  * Checks current version of this dependency
  * @example
- * const test = require("decode-and-encode-binary-text")
+ * const test = require("decode-encode-binary")
  * console.log(test.version())
  * // outputs current this dependency version
  */
@@ -52,7 +51,7 @@ version:function(){try{return require("../package.json").version;}catch(e){throw
  * Automatically detect if you want to decode or encode 
  * @param {string} detect
  * @example
- * var test = require("decode-and-encode-binary-text")
+ * var test = require("decode-encode-binary")
  * console.log(test.auto("Hello"))
  * // outputs "0100100001100101011011000110110001101111"
  * 
