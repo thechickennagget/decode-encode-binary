adc3bd9d97e10069e7b73dd595f961819053ceac
TheChickenNagget <54007132+TheChickenNagget@users.noreply.github.com>
2019-09-20 20:44:21 +0200

    added semicolon so the fucking codeacy will be okay and not panic like its end

--- src/cli-loader.js
+++ src/cli-loader.js
@@ -2,7 +2,7 @@
 'use strict';
 require("v8-compile-cache");
 var argv = process.argv.slice(2).toString();
-argv = argv.replace(",", "")
+argv = argv.replace(",", "");
 try{
 if(argv.search("encode")===0){argv=argv.replace('encode','');console.log(require("./index.js").encode(`${argv}`));process.exit(0);}
 if(argv.search("decode")===0){argv=argv.replace('decode','');if(argv === null ||  /^\s*$/.test(argv)){return console.error("> No Text to decode was provided");} console.log(require("./index.js").decode(`${argv}`));process.exit(0);}
